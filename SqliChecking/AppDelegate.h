0248ef60cd158485cd7fb9f2eeee036d88d88313
Lina Ait Khouya <Lina@MacBook-Pro-de-Lina.local>
2017-07-25 12:34:36 +0100

    Initial Commit

---
+++ SqliChecking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SqliChecking
+//
+//  Created by Lina Ait Khouya on 25/07/2017.
+//  Copyright © 2017 Lina Ait Khouya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SqliChecking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SqliChecking
+//
+//  Created by Lina Ait Khouya on 25/07/2017.
+//  Copyright © 2017 Lina Ait Khouya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
