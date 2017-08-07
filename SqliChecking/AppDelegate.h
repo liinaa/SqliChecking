f8449bbc9ee376349b074053709cad9b5e730fe2
liinaa <musicandknowledge95@gmail.com>
2017-08-07 11:23:07 +0100

    version 5

--- SqliChecking/AppDelegate.h
+++ SqliChecking/AppDelegate.h
@@ -12,9 +12,7 @@
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-@property (readonly, strong) NSPersistentContainer *persistentContainer;
 
-- (void)saveContext;
 
 
 
